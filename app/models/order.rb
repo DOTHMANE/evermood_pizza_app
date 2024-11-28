36ad5649173ef07704eb7ac9ac07e13b747b3e25
DOTHMANE <delassi.othmane03@gmail.com>
2024-11-28 12:15:58 +0100

    add index order views and total_price method

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,4 +6,8 @@ class Order < ApplicationRecord
   enum state: { COMPLETE: 1, OPEN: 0 }
 
   validates :state, presence: true
+
+  def total_price
+    @total_price ||= OrderPriceCalculator.new(self).calculate_total_price
+  end
 end
