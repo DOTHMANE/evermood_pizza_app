e79a7743ac2bf84454f07fc436f076d213559fea
DOTHMANE <delassi.othmane03@gmail.com>
2024-11-28 11:14:22 +0100

    add items relation and state enum, validation for order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class Order < ApplicationRecord
+  has_many :items, dependent: :destroy
+
+  enum state: { COMPLETE: 1, OPEN: 0 }
+
+  validates :state, presence: true
 end
