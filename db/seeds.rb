d17ecf6f43b3b8b32e415fc515da24991bb822ec
DOTHMANE <delassi.othmane03@gmail.com>
2024-11-28 11:17:13 +0100

    add db seeds data from orders json file

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,24 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'json'
+
+file_path = Rails.root.join('data/orders.json')
+orders = JSON.parse(File.read(file_path))
+
+orders.each do |order_data|
+  order = Order.create!(
+    state: order_data['state'].upcase,
+    promotion_codes: order_data['promotionCodes'],
+    discount_code: order_data['discountCode']
+  )
+
+  order_data['items'].each do |item_data|
+    order.items.create!(
+      name: item_data['name'],
+      size: item_data['size'].downcase,
+      add: item_data['add'],
+      remove: item_data['remove']
+    )
+  end
+end
