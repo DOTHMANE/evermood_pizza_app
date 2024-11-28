534807bb298042b3b0d629c0c64240eb976dd1fb
DOTHMANE <delassi.othmane03@gmail.com>
2024-11-28 12:31:08 +0100

    add update action to complete order with the view update

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,20 @@
 # frozen_string_literal: true
 
 class OrdersController < ApplicationController
+  before_action :set_order, only: [:update]
   # GET /orders
   def index
-    @orders = Order.where(state: 'COMPLETE')
+    @orders = Order.where(state: 'OPEN')
+  end
+
+  def update
+    @order.update(state: 1)
+    redirect_to orders_path
+  end
+
+  private
+
+  def set_order
+    @order = Order.find(params[:id])
   end
 end
